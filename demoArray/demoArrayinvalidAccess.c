431654d76d484ad54f6145d5d37515712c482fd3
mr_huang <mr_huang@163.com>
2023-12-08 16:16:07 +0800

    [新增] 数组的非法访问 和 静态数组的优劣势

---
+++ demoArray/demoArrayinvalidAccess.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define BUFFER_SIZE 10
+
+int main()
+{
+#if 0
+    int array[BUFFER_SIZE] = { 0 };
+
+    /* 数组的越界访问 */
+    /* 判断索引的有效性: 索引必须是合理的范围RANGE [0 - BUFFER_SIZE -1] */
+    printf("array[-1]: %d\n", array[-1]);
+    printf("array[10]: %d\n", array[BUFFER_SIZE]);
+#endif
+
+#if 1
+    int desk[BUFFER_SIZE];  /* 静态数组 */
+
+
+#endif
+
+#if 1
+    /* 字符数组 */
+
+
+
+
+#endif
+
+
+    return 0;
+}
