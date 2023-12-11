f73b25d5f0cceacdf91bc0c9c456b0efcc97bb62
mr_huang <mr_huang@163.com>
2023-12-11 11:14:03 +0800

    [新增] 新增一个字符串

---
+++ string/demoString.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUFFER_SIZE 16
+
+
+/* 作业1： 手写字符串 strlen strcat strcmp strcpy */
+int main()
+{
+
+#if 0
+    /* 字符数组 */
+    char array[BUFFER_SIZE];
+    /* 数组初始化 */
+    memset(array, 0, sizeof(array));
+
+
+    for(int idx = 0; idx < BUFFER_SIZE; idx++)
+    {
+        array[idx];
+    }
+#endif
+
+#if 0
+  /* 字符数组 */
+    char array[BUFFER_SIZE] = "hello word";
+    int len = sizeof(array);
+    printf("len:%d, array:%s\n", len, array);
+#endif
+
+#if 0
+
+    char array[BUFFER_SIZE] = "hello world";
+    /* 第一个 strlen */
+    int len = strlen(array);
+    printf("len:%d\n", len);
+
+    /* 第二个 strcpy */
+    char name[BUFFER_SIZE];
+    memset(name, 0, sizeof(name));
+    strcpy(name,"zhangsan");
+    printf("name:%s\n", name);
+    
+    /* 第三个 strcat */
+    strcat(name, " is a boy");
+    printf("name:%s\n", name);
+
+    /* 第四个 strcmp */
+    char name2[BUFFER_SIZE] = "wangwu";
+    int ret = strcmp(name,name2);
+    printf("ret:%d\n", ret); //比较
+    
+
+#endif
+
+
+#if 0
+    /* 字符串 */
+    /* 指针出入 */
+    char * ptr = "hello world";
+    int len = strlen(ptr);
+    int size = sizeof(ptr);
+
+    printf("len:%d\n", len);
+    printf("size:%d\n", size);
+
+    printf("*ptr:%c\t*(ptr+1):%c\tptr:%s\n", *ptr, *(ptr + 1), ptr);
+
+    *ptr = 'H';
+    printf("*ptr:%c\t*(ptr+1):%c\tptr:%s\n", *ptr, *(ptr + 1), ptr);
+
+#endif
+
+
+    return 0;
+}
