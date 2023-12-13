645d0590ffc9d8c19f92efedffeb0e9bab6034e1
mr_huang <mr_huang@163.com>
2023-12-13 10:10:14 +0800

    [新增] makefile测试

--- demoMakefile/main.c
+++ demoMakefile/main.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int printfString()
-{
-    int ret = 0;
-    printf("print string\n");
-    return ret;
-}
+
 int main()
 {
-    int num1 = 10;
-    printf("num1:%d\n", num1);
-    printfString();
+    printf("hello world\n");
 
-    return 0;
+return 0;
 }
